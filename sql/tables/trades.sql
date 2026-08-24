37968db9c08803b776e6437332f945adcbeb68d6
reycardo <reycardo_95@hotmail.com>
2026-08-24 10:21:32 +0100

    using hours instead of a timestamp

--- sql/tables/trades.sql
+++ sql/tables/trades.sql
@@ -11,11 +11,11 @@ CREATE TABLE trades (
 );
 
 INSERT INTO trades (id, item, item_rarity, item_effect, trade_timestamp, buyer, seller) VALUES
-  (1, 'Iron Dagger', 'common', 'rusted', '2026-06-12 18:51:09', 'MoonPriest', 'GromByte'),
-  (2, 'Steel Mace', 'uncommon', 'electrified', '2026-06-12 18:53:42', 'ShieldTotem', 'RuneTank'),
-  (3, 'Worn Sword', 'common', 'burning', '2026-06-12 18:56:27', 'xCalibur', 'ArthasMain'),
-  (4, 'Oak Buckler', 'common', NULL, '2026-06-12 18:58:11', 'EluneKid', 'VoidCook'),
-  (5, 'Hunter Axe', 'uncommon', 'rusted', '2026-06-12 19:00:04', 'GromByte', 'MoonPriest'),
-  (6, 'Silver Ring', 'rare', NULL, '2026-06-12 19:01:35', 'RuneTank', 'ShieldTotem'),
-  (7, 'Runed Cloak', 'rare', NULL, '2026-06-12 19:03:18', 'ArthasMain', 'EluneKid'),
-  (8, 'dagger', 'legendary', 'poisoned', '2026-06-12 19:09:05', 'easily_gullible123', 'VoidCook');
+  (1, 'Iron Dagger', 'common', 'rusted', '18:51:09', 'MoonPriest', 'GromByte'),
+  (2, 'Steel Mace', 'uncommon', 'electrified', '18:53:42', 'ShieldTotem', 'RuneTank'),
+  (3, 'Worn Sword', 'common', 'burning', '18:56:27', 'xCalibur', 'ArthasMain'),
+  (4, 'Oak Buckler', 'common', NULL, '18:58:11', 'EluneKid', 'VoidCook'),
+  (5, 'Hunter Axe', 'uncommon', 'rusted', '19:00:04', 'GromByte', 'MoonPriest'),
+  (6, 'Silver Ring', 'rare', NULL, '19:01:35', 'RuneTank', 'ShieldTotem'),
+  (7, 'Runed Cloak', 'rare', NULL, '19:03:18', 'ArthasMain', 'EluneKid'),
+  (8, 'dagger', 'legendary', 'poisoned', '19:09:05', 'easily_gullible123', 'VoidCook');
